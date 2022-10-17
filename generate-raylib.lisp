40e4bc0a1eb838e5f74a8c7258e0ae3d43361514
vldn <4529781+nullscm@users.noreply.github.com>
2022-10-17 12:40:45 +0200

    Color generation based on xml instead of hard defined values

--- generate-raylib.lisp
+++ generate-raylib.lisp
@@ -1,36 +1,37 @@
-(import (file xml) (owl string) (scheme file))
-(define (tokenize l)
-  (let loop ((t '()) (l l))
-    (if (pair? l)
-      (let ((c (car l)))
-        (if (char=? c #\space)
-          (cons (reverse t) (loop '() (cdr l)))
-          (loop (cons (car l) t) (cdr l))))
-      (if (null? t) '() (list (reverse t))))))
-(define (string-split s)
-  (map list->string (tokenize (string->list s))))
-(if (not (file-exists? "raylib_api.xml"))
-  (syscall
-    1017
-    (c-string
-      "wget https://raw.githubusercontent.com/raysan5/raylib/master/parser/output/raylib_api.xml")
-    #f
-    #f))
-(define xml
-  (xml-get-root-element
-    (xml-parse-file "raylib_api.xml")))
+(import (file xml)(scheme file))
+
+;; filter numbers from string
+(define (digit? x) (<= #\0 x #\9))
+(define (number-filter lst)
+  (cond ((null? lst) lst)
+        ((or (eq? #\space (car lst)) (digit? (car lst))) (cons (car lst) (number-filter (cdr lst)))) 
+        (else
+          (number-filter (cdr lst)))))
+(define filter-numbers (lambda (lst) (list->string (number-filter (string->list lst)))))
+
+;; tokenize string based on #\space
+(define (tokenize l) (let loop ((t '()) (l l)) (if (pair? l) (let ((c (car l))) (if (char=? c #\space) (cons (reverse t) (loop '() (cdr l))) (loop (cons (car l) t) (cdr l)))) (if (null? t) '() (list (reverse t))))))
+
+;; split string by #\space
+(define (string-split s) (map list->string (tokenize (string->list s))))
+
+;; Todo: Replace in the future/simplify
+(define (getty x y) (cdr (assoc x (ff->alist (cadr (vector->list y))))))
+(define (show x y port) (if (not (equal? (getty x y) #f)) (display (getty x y) port)))
+
+;; download raylib_api.xml
+(if (not (file-exists? "raylib_api.xml")) (syscall 1017 (c-string "wget https://raw.githubusercontent.com/raysan5/raylib/master/parser/output/raylib_api.xml") #f #f))
+
+;; load raylib_api.xml
+(define xml (xml-get-root-element (xml-parse-file "raylib_api.xml")))
 (define enums (xml-get-subtags xml 'Enums))
-(define enums
-  (xml-get-subtags (ref enums 1) 'Enum))
+(define enums (xml-get-subtags (ref enums 1) 'Enum))
 (define defines (xml-get-subtags xml 'Defines))
-(define defines
-  (xml-get-subtags (ref defines 1) 'Define))
+(define defines (xml-get-subtags (ref defines 1) 'Define))
 (define aliases (xml-get-subtags xml 'Aliases))
-(define aliases
-  (xml-get-subtags (ref aliases 1) 'Alias))
+(define aliases (xml-get-subtags (ref aliases 1) 'Alias))
 (define funcs (xml-get-subtags xml 'Functions))
-(define funcs
-  (xml-get-subtags (ref funcs 1) 'Function))
+(define funcs (xml-get-subtags (ref funcs 1) 'Function))
 (define funcs
   (filter
     (lambda (func)
@@ -39,99 +40,38 @@
       (or (string-eq? "void" refType)
           (string-eq? "bool" refType)))
     funcs))
-(define (getty x y)
-  (cdr (assoc x (ff->alist (cadr (vector->list y))))))
-(define (show x y port)
-  (if (not (equal? (getty x y) #f))
-    (display (getty x y) port)))
-(define %param-blacklist
-  (list "Ray"
-        "Ray *"
-        "Camera *"
-        "Rectangle"
-        "Texture2D"
-        "Texture2D *"
-        "GlyphInfo *"
-        "GlyphInfo"
-        "Camera"
-        "Camera2D"
-        "Camera3D"
-        "Font"
-        "Vector2 *"
-        "Color"
-        "Image *"
-        "Image"
-        "Vector3"
-        "Mesh *"
-        "Model"
-        "BoundingBox"
-        "ModelAnimation"
-        "Model *"
-        "ModelAnimation *"
-        "Vector3 *"
-        "Sound"
-        "Wave"
-        "Material"
-        "Matrix"
-        "Music"
-        "AudioStream"
-        "AudioCallback"
-        "Color *"
-        "Shader"
-        "RenderTexture2D"
-        "VrStereoConfig"
-        "..."
-        "Mesh"
-        "TraceLogCallback"
-        "SaveFileTextCallback"
-        "Material *"
-        "bool"
-        "SaveFileDataCallback"
-        "LoadFileTextCallback"
-        "NPatchInfo"
-        "Wave *"
-        "const Matrix *"
-        "LoadFileDataCallback"
-        "FilePathList"))
+
+(define %convert-to-fft-enum
+  (list "Ray" "Ray *" "Camera *" "Rectangle" "Texture2D" "Texture2D *" "GlyphInfo *" "GlyphInfo" "Camera" "Camera2D" "Camera3D" "Font" "Vector2 *" "Color" "Image *" "Image" "Vector3" "Mesh *" "Model" "BoundingBox" "ModelAnimation" "Model *" "ModelAnimation *" "Vector3 *" "Sound" "Wave" "Material" "Matrix" "Music" "AudioStream" "AudioCallback" "Color *" "Shader" "RenderTexture2D" "VrStereoConfig" "..." "Mesh" "TraceLogCallback" "SaveFileTextCallback" "Material *" "bool" "SaveFileDataCallback" "LoadFileTextCallback" "NPatchInfo" "Wave *" "const Matrix *" "LoadFileDataCallback" "FilePathList"))
+
 (define (show-param-types params port)
   (for-each
     (lambda (param)
       (let* ((t (assoc 'type (ff->alist (ref param 2))))
              (type (if (pair? t) (cdr t) t)))
-        (cond ((member type %param-blacklist)
-               (display " fft-enum" port))
-              ((equal? "const char *" type)
-               (display " type-string" port))
-              ((equal? "float *" type)
-               (display " fft-float" port))
-              ((equal? "void *" type)
-               (display " fft-void*" port))
-              ((equal? "const void *" type)
-               (display " fft-void*" port))
-              ((equal? "const int *" type)
-               (display " fft-int*" port))
-              ((equal? "int *" type)
-               (display " fft-int*" port))
-              ((equal? "char *" type)
-               (display " fft-char" port))
-              ((equal? "Vector2" type)
-               (display " Vector2" port))
+        (cond ((member type %convert-to-fft-enum) (display " fft-enum" port))
+              ((equal? "const char *" type) (display " type-string" port))
+              ((equal? "float *" type) (display " fft-float" port))
+              ((equal? "void *" type) (display " fft-void*" port))
+              ((equal? "const void *" type) (display " fft-void*" port))
+              ((equal? "const int *" type) (display " fft-int*" port))
+              ((equal? "int *" type) (display " fft-int*" port))
+              ((equal? "char *" type) (display " fft-char" port))
+              ((equal? "Vector2" type) (display " Vector2" port))
               ((equal? "unsigned" (car (string-split type)))
-               (display
-                 (string-append
-                   " fft-"
-                   (car (string-split type))
-                   "-"
-                   (cadr (string-split type)))
-                 port))
+               (display-to port (string-append " fft-" (car (string-split type)) "-" (cadr (string-split type)))))
               (else
-               (display (string-append " fft-" type) port)))))
+                (display-to port (string-append " fft-" type) )))))
     (car (cddr (vector->list params)))))
+
+;; get void and bool names
 (define (generate-functionnames port)
   (for-each
     (lambda (x)
       (begin (show 'name x port) (display "\n" port)))
     funcs))
+
+;; void generator
 (define (generate-functions port)
   (for-each
     (lambda (x)
@@ -146,82 +86,62 @@
         (show-param-types x port)
         (display "))\n" port)))
     funcs))
-(define %colors
-  '((LIGHTGRAY 200 200 200 255)
-    (GRAY 130 130 130 255)
-    (DARKGRAY 80 80 80 255)
-    (YELLOW 253 249 0 255)
-    (GOLD 255 203 0 255)
-    (ORANGE 255 161 0 255)
-    (PINK 255 109 194 255)
-    (RED 230 41 55 255)
-    (MAROON 190 33 55 255)
-    (GREEN 0 228 48 255)
-    (LIME 0 158 47 255)
-    (DARKGREEN 0 117 44 255)
-    (SKYBLUE 102 191 255 255)
-    (BLUE 0 121 241 255)
-    (DARKBLUE 0 82 172 255)
-    (PURPLE 200 122 255 255)
-    (VIOLET 135 60 190 255)
-    (DARKPURPLE 112 31 126 255)
-    (BEIGE 211 176 131 255)
-    (BROWN 127 106 79 255)
-    (DARKBROWN 76 63 47 255)
-    (WHITE 255 255 255 255)
-    (BLACK 0 0 0 255)
-    (BLANK 0 0 0 0)
-    (MAGENTA 255 0 255 255)
-    (RAYWHITE 245 245 245 255)))
-(define (generate-colors port)
-  (for-each
-    (lambda (x)
-      (display "(define " port)
-      (display (car x) port)
-      (display " (rgba->hex" port)
-      (for-each
-        (lambda (y) (display " " port) (display y port))
-        (cdr x))
-      (display "))" port))
-    %colors))
 
-(syscall 1017 (c-string "mkdir lib") #f #f)
+;;;; Generate lib/raylib.scm file
+;; create ./lib directory
+(if (not (file-exists? "lib")) (syscall 1017 (c-string "mkdir lib") #f #f))
 (define port (open-output-file "lib/raylib.scm"))
-(display
-  "(define-library (lib raylib) (import (otus lisp) (otus ffi)) (export \n"
-  port)
-(for-each
-  (lambda (x)
-    (display (car x) port)
-    (display "\n" port))
-  %colors)
+
+;; Define Library
+(print-to port "(define-library (lib raylib) (import (otus lisp) (otus ffi)) (export ")
+
+;; Colornames
+(for-each 
+  (lambda (define) 
+    (if (equal? "COLOR" (xml-get-attribute define 'type #f)) (print-to port (xml-get-attribute define 'name #f))))
+  defines)
+
 ;; Enums
 (for-each (lambda (enum)
-      (for-each (lambda (value)
-            (print-to port (xml-get-attribute value 'name "")))
-         (xml-get-subtags enum 'Value)))
-   enums)
+            (for-each (lambda (value)
+                        (print-to port (xml-get-attribute value 'name "")))
+                      (xml-get-subtags enum 'Value)))
+          enums)
 
 (display "rgba->hex \n" port)
+
+;; Functionnames
 (generate-functionnames port)
-(print-to port
-  ")(cond-expand (Linux (begin (define raylib (load-dynamic-library \"libraylib.so\")) (define raylib-err \"Use, for example, sudo apt install libraylib.so\"))) (else (runtime-error \"nsupported platform\" (uname)))) (begin (if (not raylib) (runtime-error \"Can't load raylib library.\" raylib-err))")
-(print-to port
-  "(define Vector2 (list fft-float fft-float))")
+(print-to port ")(cond-expand (Linux (begin (define raylib (load-dynamic-library \"libraylib.so\")) (define raylib-err \"Use, for example, sudo apt install libraylib.so\"))) (else (runtime-error \"nsupported platform\" (uname)))) (begin (if (not raylib) (runtime-error \"Can't load raylib library.\" raylib-err))")
+
+;; Vector2 
+(print-to port) (print-to port "(define Vector2 (list fft-float fft-float))")
+
+;; rgba->hex
+(print-to port) (print-to port "(define (rgba->hex r g b a) (+ (<< a 24) (<< b 16) (<< g 8) r))")
+
+;; Functions
+(print-to port) (print-to port ";;;; Voids & Bools")
 (generate-functions port)
-(print-to port
-  "(define (rgba->hex r g b a) (+ (<< a 24) (<< b 16) (<< g 8) r))")
 
-; Defines
-(print-to port)
-(print-to port ";;;; Enums")
+;; Enums
+(print-to port) (print-to port ";;;; Enums")
 (for-each (lambda (enum)
-      (print-to port ";; " (xml-get-attribute enum 'name #f))
-      (for-each (lambda (value)
-            (print-to port "(define " (xml-get-attribute value 'name "") " " (xml-get-attribute value 'integer "") ")"))
-         (xml-get-subtags enum 'Value)))
-   enums)
+            (print-to port ";; " (xml-get-attribute enum 'name #f))
+            (for-each (lambda (value)
+                        (print-to port "(define " (xml-get-attribute value 'name "") " " (xml-get-attribute value 'integer "") ")"))
+                      (xml-get-subtags enum 'Value)))
+          enums)
 
-(generate-colors port)
+;; Colors
+(print-to port) (print-to port ";;;; Colors")
+(for-each 
+  (lambda (define)
+    (cond ((equal? "COLOR" (xml-get-attribute define 'type #f))
+           (print-to port "(define " (xml-get-attribute define 'name #f) " (rgba->hex " 
+                     (filter-numbers (xml-get-attribute define 'value #f)) "))")))) 
+  defines)
 (display "))" port)
+
 (close-port port)
+
